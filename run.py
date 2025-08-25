c084304e916f0bca7aff6f961d2bc672077c9391
d-angelotorres <dangelotorres2@gmail.com>
2025-08-25 02:38:51 -0400

    fix(ci): correct Docker container test port and ensure Flask listens on 0.0.0.0 for CI/CD

--- run.py
+++ run.py
@@ -5,5 +5,6 @@ from app import create_app
 app = create_app()
 
 # Run the app only if this file is executed directly
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
